8a4775813d0cf54d5417be6bf3ade705aba8f261
Manuel Duran <mduran85@gmail.com>
2020-06-22 14:10:37 -0500

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  let highNum = 0;
+
+  for (let char in charMap) {
+    if (charMap[char] > highNum) {
+      highNum = char;
+    }
+  }
+
+  return highNum;
+}
 
 module.exports = maxChar;
