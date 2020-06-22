ac7a5bd994c13b8be7e926e719d0528441adb279
Manuel Duran <mduran85@gmail.com>
2020-06-22 15:06:16 -0500

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,30 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let stringAMap = makeMap(stringA);
+  let stringBMap = makeMap(stringB);
+
+  if (Object.keys(stringAMap).length !== Object.keys(stringBMap).length) {
+    return false;
+  }
+
+  for (let char in stringAMap) {
+    if (stringAMap[char] !== stringBMap[char]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function makeMap(string) {
+  let map = {};
+
+  for (let char of string) {
+    if (char.match(/[A-Z]/gi)) map[char] = map[char] + 1 || 1;
+  }
+
+  return map;
+}
 
 module.exports = anagrams;
