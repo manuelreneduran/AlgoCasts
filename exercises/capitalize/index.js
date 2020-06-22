f1d6e53d97cb1ec12f16e5ba9db34677f639cd94
Manuel Duran <mduran85@gmail.com>
2020-06-22 15:26:01 -0500

    better solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,13 +8,22 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  let sentence = str.split(" ");
-  for (var i = 0; i < sentence.length; i++) {
-    var word = sentence[i];
-    word = word[0].toUpperCase() + word.slice(1);
-    sentence[i] = word;
+  var words = [];
+
+  for (var word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
   }
-  return sentence.join(" ");
+
+  return words.join(" ");
 }
+// function capitalize(str) {
+//   let sentence = str.split(" ");
+//   for (var i = 0; i < sentence.length; i++) {
+//     var word = sentence[i];
+//     word = word[0].toUpperCase() + word.slice(1);
+//     sentence[i] = word;
+//   }
+//   return sentence.join(" ");
+// }
 
 module.exports = capitalize;
