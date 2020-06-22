87dbdc6302a6ecf0dbdbbc23747e49005a05cccd
Manuel Duran <mduran85@gmail.com>
2020-06-22 14:53:41 -0500

    chunks

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,31 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //using slice
+  let chunks = [];
+  let minPos = 0;
+
+  while (minPos < array.length) {
+    chunks.push(array.slice(minPos, minPos + size));
+    minPos += size;
+  }
+  return chunks;
+}
+
+// function chunk(array, size) {
+//   var chunks = [];
+//   var chunk = [];
+
+//   for (var i = 0; i < array.length; i++) {
+//     let num = array[i];
+//     chunk.push(num);
+//     if (chunk.length === size || array[i + 1] === undefined) {
+//       chunks.push(chunk);
+//       chunk = [];
+//     }
+//   }
+//   return chunks;
+// }
 
 module.exports = chunk;
