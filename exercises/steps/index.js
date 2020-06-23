0774dc5ffcd50a943057b2d4554b1037581dcf9a
Manuel Duran <mduran85@gmail.com>
2020-06-23 12:14:56 -0500

    steps better solutions

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,22 +17,61 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  var counter = 1;
-
-  for (var i = 0; i < n; i++) {
-    var step = "";
-
-    for (var q = 0; q < n; q++) {
-      if (q < counter) {
-        step += "#";
-      } else {
-        step += " ";
-      }
-    }
-    console.log(step);
-    counter++;
+function steps(n, row = 0, stair = "") {
+  if (row === n) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  steps(n, row, stair);
 }
+
+// function steps(n, step = "", counter = 1) {
+//   if (counter > n) {
+//     return null;
+//   }
+
+//   if (step.length < counter) {
+//     step += "#";
+//   } else {
+//     step += " ";
+//   }
+
+//   if (step.length === n) {
+//     console.log(step);
+//     step = "";
+//     steps(n, step, counter + 1);
+//     return null;
+//   }
+
+//   steps(n, step, counter);
+// }
+// function steps(n) {
+//   var counter = 1;
+
+//   for (var i = 0; i < n; i++) {
+//     var step = "";
+
+//     for (var q = 0; q < n; q++) {
+//       if (q < counter) {
+//         step += "#";
+//       } else {
+//         step += " ";
+//       }
+//     }
+//     console.log(step);
+//     counter++;
+//   }
+// }
 
 module.exports = steps;
