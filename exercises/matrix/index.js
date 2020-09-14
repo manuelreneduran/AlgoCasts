5f2f9648b37edac39bbf3608fc77013b69b2af12
Manuel Duran <mduran85@gmail.com>
2020-09-14 07:34:46 -0500

    completed matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,76 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(N) {
+  let spiral = []
 
-module.exports = matrix;
+  for (var i = 0; i < N; i++) {
+    spiral.push(Array.from(Array(N), (_, ind) => 'n'))
+  }
+
+  let max = N * N
+  let counter = 1
+
+  let colInd = 0
+  let rowInd = 0
+
+  while (counter <= max) {
+    let direction = ''
+    spiral[rowInd][colInd] = counter
+    if (
+      spiral[rowInd][colInd + 1] &&
+      spiral[rowInd][colInd + 1] === 'n' &&
+      (!spiral[rowInd - 1] || spiral[rowInd - 1][colInd] !== 'n')
+    ) {
+      direction = 'right'
+    }
+
+    if (
+      spiral[rowInd + 1] &&
+      spiral[rowInd + 1][colInd] === 'n' &&
+      (!spiral[rowInd][colInd + 1] || spiral[rowInd][colInd + 1] !== 'n')
+    ) {
+      direction = 'down'
+    }
+
+    if (
+      spiral[rowInd][colInd - 1] &&
+      spiral[rowInd][colInd - 1] === 'n' &&
+      (!spiral[rowInd + 1] || spiral[rowInd + 1][colInd] !== 'n')
+    ) {
+      direction = 'left'
+    }
+
+    if (
+      spiral[rowInd - 1] &&
+      spiral[rowInd - 1][colInd] === 'n' &&
+      (!spiral[rowInd][colInd - 1] || spiral[rowInd][colInd - 1] !== 'n')
+    ) {
+      direction = 'up'
+    }
+
+    switch (direction) {
+      case 'right':
+        colInd++
+        break
+
+      case 'down':
+        rowInd++
+        break
+
+      case 'left':
+        colInd--
+        break
+
+      case 'up':
+        rowInd--
+        break
+    }
+
+    counter++
+  }
+
+  return spiral
+}
+
+module.exports = matrix
